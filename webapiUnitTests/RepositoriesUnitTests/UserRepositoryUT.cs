4c5a1de25790935f3b40cf64279828636e9693d8
KonradLitwinowicz <56590192+Sendej7@users.noreply.github.com>
2023-09-15 23:45:01 +0200

    Adding new tests

--- webapiUnitTests/RepositoriesUnitTests/UserRepositoryUT.cs
+++ webapiUnitTests/RepositoriesUnitTests/UserRepositoryUT.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using webapi.Data;
 using webapi.Interfaces;
 using webapi.Models;
+using webapi.Models.DND;
 using webapi.Repositories;
 using Xunit;
 
@@ -52,6 +53,69 @@ namespace webapiUnitTests.RepositoriesUnitTests
                 retrievedUser = await userRepository.GetUserByIdAsync(1);
             }
             Assert.Null(retrievedUser);
+        }
+        [Fact]
+        public async Task UpdateUserAndCharacterRelationship_ShouldReturnTrue_WhenBothExist()
+        {
+            var options = new DbContextOptionsBuilder<CharSheetContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+            using (var context = new CharSheetContext(options))
+            {
+                context.BaseCharacters.Add(new User { ID = 1, UserToken = 1 });
+                context.DNDCharacters.Add(new DndCharacter { ID = 1, UserToken = 1, CharacterName = "John" });
+                await context.SaveChangesAsync();
+            }
+
+            bool result;
+            using (var context = new CharSheetContext(options))
+            {
+                var userRepository = new UserRepository(context);  
+                result = await userRepository.UpdateUserAndCharacterRelationship(1, 1);
+            }
+
+            Assert.True(result);
+        }
+        [Fact]
+        public async Task UpdateUserAndCharacterRelationship_ShouldReturnFalse_WhenUserDoesNotExist()
+        {
+            var options = new DbContextOptionsBuilder<CharSheetContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+            bool result;
+            using (var context = new CharSheetContext(options))
+            {
+                var userRepository = new UserRepository(context); 
+                result = await userRepository.UpdateUserAndCharacterRelationship(1, 1);
+            }
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task UpdateUserAndCharacterRelationship_ShouldReturnFalse_WhenCharacterDoesNotExist()
+        {
+            var options = new DbContextOptionsBuilder<CharSheetContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+            using (var context = new CharSheetContext(options))
+            {
+                context.BaseCharacters.Add(new User { ID = 1, UserToken = 1 });
+                await context.SaveChangesAsync();
+            }
+
+            bool result;
+            using (var context = new CharSheetContext(options))
+            {
+                var userRepository = new UserRepository(context); 
+                result = await userRepository.UpdateUserAndCharacterRelationship(1, 1);
+            }
+
+            // Assert
+            Assert.False(result);
         }
     }
 }
