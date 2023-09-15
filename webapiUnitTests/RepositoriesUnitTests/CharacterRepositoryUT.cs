60acae683774f338f7168936417766eacde59db6
KonradLitwinowicz <56590192+Sendej7@users.noreply.github.com>
2023-09-15 22:06:19 +0200

    New Tests

--- webapiUnitTests/RepositoriesUnitTests/CharacterRepositoryUT.cs
+++ webapiUnitTests/RepositoriesUnitTests/CharacterRepositoryUT.cs
@@ -232,5 +232,6 @@ namespace webapiUnitTests.RepositoriesUnitTests
                 await Assert.ThrowsAsync<NullReferenceException>(() => characterSheetRepository.CreateCharacterAsync(nullCharacter!));
             }
         }
+
     }
 }
